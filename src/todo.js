17846faba920573b16bf4dee97bf15a03b4184f0
Michał Jezierski <ambinanitelo@gmail.com>
2015-10-12 13:07:28 +0200

    remove unused, generated files

--- src/todo.js
+++
@@ -1,9 +0,0 @@
-var Todo = (function () {
-    function Todo(text) {
-        this.text = text;
-        this.completed = false;
-    }
-    return Todo;
-})();
-exports.Todo = Todo;
-//# sourceMappingURL=todo.js.map
